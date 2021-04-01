8a15f4f8a78c24c1de4005ed499b9563aca2c0c8
Yoon <yoondev93@gmail.com>
2021-04-01 17:45:15 +0900

    feat: Add Job Conditional example code

--- src/main/java/com/spring/batch/job/StepNextJobConfiguration.java
+++ src/main/java/com/spring/batch/job/StepNextJobConfiguration.java
@@ -1,7 +1,5 @@
 package com.spring.batch.job;
 
-import java.util.Date;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -17,39 +15,45 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
-public class JobConfiguration {
+public class StepNextJobConfiguration {
 
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
 
   @Bean
-  public Job simpleJob() {
-    return jobBuilderFactory.get("빌링")
-        .start(step1(null))
-        .next(step2(null))
+  public Job stepNextJob() {
+    return jobBuilderFactory.get("stepNextJob")
+        .start(step1())
+        .next(step2())
+        .next(step3())
         .build();
   }
 
   @Bean
-  @JobScope
-  public Step step1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+  public Step step1() {
     return stepBuilderFactory.get("step1")
         .tasklet((contribution, chunkContext) -> {
-//          throw  new IllegalArgumentException("Step1 failed");
           log.info(">>>>> This is Step1");
-          log.info(">>>>> requestDate = {}", requestDate);
           return RepeatStatus.FINISHED;
         })
         .build();
   }
 
   @Bean
-  @JobScope
-  public Step step2(@Value("#{jobParameters[requestDate]}") String requestDate) {
-    return stepBuilderFactory.get("simpleStep2")
+  public Step step2() {
+    return stepBuilderFactory.get("step2")
         .tasklet((contribution, chunkContext) -> {
           log.info(">>>>> This is Step2");
-          log.info(">>>>> requestDate = {}", requestDate);
+          return RepeatStatus.FINISHED;
+        })
+        .build();
+  }
+
+  @Bean
+  public Step step3() {
+    return stepBuilderFactory.get("step3")
+        .tasklet((contribution, chunkContext) -> {
+          log.info(">>>>> This is Step3");
           return RepeatStatus.FINISHED;
         })
         .build();
