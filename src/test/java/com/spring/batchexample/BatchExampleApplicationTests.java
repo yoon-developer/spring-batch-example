088e2a827ea1acc63777ac012d1eb9062530edd4
신동윤 <yoondev93@gmail.com>
2023-03-16 21:38:59 +0900

    Init

--- src/test/java/com/spring/batchexample/BatchExampleApplicationTests.java
+++ src/test/java/com/spring/batchexample/BatchExampleApplicationTests.java
@@ -1,10 +1,10 @@
-package com.spring.batch;
+package com.spring.batchexample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BatchApplicationTests {
+class BatchExampleApplicationTests {
 
 	@Test
 	void contextLoads() {
